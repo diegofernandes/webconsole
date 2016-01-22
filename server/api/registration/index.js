994a18e1b03340124b0b3e805e8ae08dda8db55b
Rogerio Biondi <rogerio.biondi@gmail.com>
2016-01-22 09:56:38 -0200

    Messages and Registration Controllers

---
+++ server/api/registration/index.js
@@ -0,0 +1,34 @@
+/*
+* Meccano IOT Webconsole
+*
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+'use strict';
+
+var express = require('express');
+var controller = require('./registration.controller');
+
+var router = express.Router();
+
+// Routes for /api/registration/
+router.get('/:device', controller.get);
+router.post('/', controller.post);
+router.delete('/:device', controller.delete);
+router.put('/', controller.put);
+
+
+module.exports = router;
