24ebf2db786d4006f7fc6f351a7bc223fd268175
Rogerio Biondi <rogerio.biondi@gmail.com>
2016-02-02 15:35:36 -0200

    statusHistory API

--- server/api/deviceStatus/index.js
+++ server/api/deviceStatus/index.js
@@ -2,11 +2,14 @@
 
 var express = require('express');
 var controller = require('./deviceStatus.controller');
+var historyStatusController = require('./historyStatus.controller');
 var auth = require('../../auth/auth.service');
+
 
 var router = express.Router();
 
 router.get('/', auth.isAuthenticated(),controller.count);
 router.get('/status/:status', auth.isAuthenticated(),controller.status);
+router.get('/history', historyStatusController.history );
 
 module.exports = router;
