4d53fcf116983401b49b1ac2c6d9e790ed3e446a
Diego Osse Fernandes <diego.osse@gmail.com>
2016-02-15 16:55:18 -0200

    Fix device status history route

--- server/api/deviceStatus/index.js
+++ server/api/deviceStatus/index.js
@@ -10,8 +10,10 @@ var router = express.Router();
 
 router.get('/', auth.isAuthenticated(),controller.count);
 router.get('/status/:status',auth.isAuthenticated(), controller.show);
+router.get('/history', auth.isAuthenticated(), historyStatusController.show );
+
 router.get('/:device',auth.isAuthenticated(), controller.show);
 
-router.get('/history', auth.isAuthenticated(), historyStatusController.show );
+
 
 module.exports = router;
