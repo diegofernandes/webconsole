f79131ffad5a9a675cbaaf46b49770a7de3f626a
Rogerio Biondi <rogerio.biondi@gmail.com>
2016-04-18 14:00:58 -0300

    Plugin List and Detail Views

--- client/app/plugin/plugin.controller.js
+++ client/app/plugin/plugin.controller.js
@@ -1,35 +1,31 @@
 'use strict';
-(function(){
 
-function PluginComponent($scope) {
-  $scope.message = 'Hello';
-  // Initialize variables
-  $scope.plugin.list = {[
-    {
-      "plugin" : "dataFilter:1.0",
-      "engine" : "R",
-      "enabled" : true,
-      "schedule" : "15 * * * *",
-      "description" : "Filter data of the sensors (version 1.0)",
-      "type" : "worker",
-      "executionContext" : "both"
-    },
-    {
-      "plugin" : "deviceAvgTime:1.0",
-      "engine" : "R",
-      "enabled" : true,
-      "schedule" : "*/15 * * * *",
-      "description" : "Device Average Response Time",
-      "type" : "report",
-      "executionContext" : "both"
-    }
-  ]};
-}
 
 angular.module('meccanoAdminApp')
-  .component('plugin', {
-    templateUrl: 'app/plugin/plugin.html',
-    controller: PluginComponent
-  });
-
-})();
+  .controller('PluginListCtrl', function($scope, Registration, $state, $stateParams, Plugins) {
+    // Initialize variables
+    $scope.pageNumber = 1;
+    $scope.parametersFilter = $state.params;
+    // $scope.Plugins = Plugins;
+    $scope.Plugins = {[
+      {
+        "plugin" : "dataFilter:1.0",
+        "engine" : "R",
+        "enabled" : true,
+        "schedule" : "15 * * * *",
+        "description" : "Filter data of the sensors (version 1.0)",
+        "type" : "worker",
+        "executionContext" : "both"
+      },
+      {
+        "plugin" : "deviceAvgTime:1.0",
+        "engine" : "R",
+        "enabled" : true,
+        "schedule" : "*/15 * * * *",
+        "description" : "Device Average Response Time",
+        "type" : "report",
+        "executionContext" : "both"
+      }
+      console.log($scope);
+    ]};
+});
--- client/app/plugin/plugin.js
+++ client/app/plugin/plugin.js
@@ -5,6 +5,30 @@ angular.module('meccanoAdminApp')
     $stateProvider
       .state('plugin', {
         url: '/plugin',
-        template: '<plugin></plugin>'
+        abstract: true,
+        templateUrl: 'app/plugin/plugin.html'
+      })
+      .state('plugin.list', {
+        url: '?page&size',
+        authenticate: true,
+        views: {
+          'index': {
+            templateUrl: 'app/plugin/plugin.list.html',
+            controller: 'PluginListCtrl'
+          },
+          'details-plugin': {
+            templateUrl: 'app/plugin/plugin.detail.html',
+            controller: 'PluginDetailCtrl'
+          }
+        }
       });
-  });
+})
+.controller('PluginDetailCtrl', function($scope, $http, $state, $stateParams, $rootScope, Plugins, $uibModal, Messages,Auth, Modal,alertsPanel) {
+  $scope.series = ['Updates'];
+
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+
+  $scope.Plugins = Plugins;
+});
